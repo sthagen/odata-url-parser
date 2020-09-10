40ebc52dd9f0648b2c8aabddb25b3f27bc5a3bc8
Stefan Hagen <stefan@hagen.link>
2020-09-10 23:13:45 +0200

    Create test_cli.py

---
+++ tests/test_cli.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring,unused-import,reimported
+import io
+import pytest  # type: ignore
+
+import odata_url_parser.cli as cli
+import odata_url_parser.odata_url_parser as oup
