3c689407df6432b4ea685eba09584c9595b5dfc7
Stefan Hagen <stefan@hagen.link>
2020-09-11 21:09:38 +0200

    Initial cli test with no meaning at all

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,3 +5,11 @@ import pytest  # type: ignore
 
 import odata_url_parser.cli as cli
 import odata_url_parser.odata_url_parser as oup
+
+
+def test_main_ok_minimal(capsys):
+    job = ['does not matter']
+    report_expected = ''
+    assert cli.main(job) is None
+    out, err = capsys.readouterr()
+    assert out.strip() == report_expected.strip()
