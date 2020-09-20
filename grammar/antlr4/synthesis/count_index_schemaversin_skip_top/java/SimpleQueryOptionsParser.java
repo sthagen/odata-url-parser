209e4088ee18038ac879ae0c3a9395fb901b081d
Stefan Hagen <stefan@hagen.link>
2020-09-20 15:48:58 +0200

    Added corrected generated java code

--- grammar/antlr4/synthesis/count_index_schemaversin_skip_top/java/SimpleQueryOptionsParser.java
+++ grammar/antlr4/synthesis/count_index_schemaversin_skip_top/java/SimpleQueryOptionsParser.java
@@ -244,10 +244,10 @@ public class SimpleQueryOptionsParser extends Parser {
 	}
 
 	public static class InlinecountContext extends ParserRuleContext {
-		public TerminalNode DOLLAR() { return getToken(SimpleQueryOptionsParser.DOLLAR, 0); }
 		public TerminalNode COUNT() { return getToken(SimpleQueryOptionsParser.COUNT, 0); }
 		public TerminalNode EQ() { return getToken(SimpleQueryOptionsParser.EQ, 0); }
 		public TerminalNode BOOLEANVALUE() { return getToken(SimpleQueryOptionsParser.BOOLEANVALUE, 0); }
+		public TerminalNode DOLLAR() { return getToken(SimpleQueryOptionsParser.DOLLAR, 0); }
 		public InlinecountContext(ParserRuleContext parent, int invokingState) {
 			super(parent, invokingState);
 		}
@@ -265,16 +265,25 @@ public class SimpleQueryOptionsParser extends Parser {
 	public final InlinecountContext inlinecount() throws RecognitionException {
 		InlinecountContext _localctx = new InlinecountContext(_ctx, getState());
 		enterRule(_localctx, 4, RULE_inlinecount);
+		int _la;
 		try {
 			enterOuterAlt(_localctx, 1);
 			{
-			setState(29);
-			match(DOLLAR);
 			setState(30);
-			match(COUNT);
-			setState(31);
-			match(EQ);
+			_errHandler.sync(this);
+			_la = _input.LA(1);
+			if (_la==DOLLAR) {
+				{
+				setState(29);
+				match(DOLLAR);
+				}
+			}
+
 			setState(32);
+			match(COUNT);
+			setState(33);
+			match(EQ);
+			setState(34);
 			match(BOOLEANVALUE);
 			}
 		}
@@ -290,9 +299,9 @@ public class SimpleQueryOptionsParser extends Parser {
 	}
 
 	public static class Index_Context extends ParserRuleContext {
-		public TerminalNode DOLLAR() { return getToken(SimpleQueryOptionsParser.DOLLAR, 0); }
 		public TerminalNode INDEX() { return getToken(SimpleQueryOptionsParser.INDEX, 0); }
 		public TerminalNode EQ() { return getToken(SimpleQueryOptionsParser.EQ, 0); }
+		public TerminalNode DOLLAR() { return getToken(SimpleQueryOptionsParser.DOLLAR, 0); }
 		public TerminalNode DASH() { return getToken(SimpleQueryOptionsParser.DASH, 0); }
 		public List<TerminalNode> DIGIT() { return getTokens(SimpleQueryOptionsParser.DIGIT); }
 		public TerminalNode DIGIT(int i) {
@@ -319,33 +328,41 @@ public class SimpleQueryOptionsParser extends Parser {
 		try {
 			enterOuterAlt(_localctx, 1);
 			{
-			setState(34);
-			match(DOLLAR);
-			setState(35);
+			setState(37);
+			_errHandler.sync(this);
+			_la = _input.LA(1);
+			if (_la==DOLLAR) {
+				{
+				setState(36);
+				match(DOLLAR);
+				}
+			}
+
+			setState(39);
 			match(INDEX);
-			setState(36);
+			setState(40);
 			match(EQ);
-			setState(38);
+			setState(42);
 			_errHandler.sync(this);
 			_la = _input.LA(1);
 			if (_la==DASH) {
 				{
-				setState(37);
+				setState(41);
 				match(DASH);
 				}
 			}
 
-			setState(41); 
+			setState(45); 
 			_errHandler.sync(this);
 			_la = _input.LA(1);
 			do {
 				{
 				{
-				setState(40);
+				setState(44);
 				match(DIGIT);
 				}
 				}
-				setState(43); 
+				setState(47); 
 				_errHandler.sync(this);
 				_la = _input.LA(1);
 			} while ( _la==DIGIT );
@@ -363,10 +380,10 @@ public class SimpleQueryOptionsParser extends Parser {
 	}
 
 	public static class SchemaversionContext extends ParserRuleContext {
-		public TerminalNode DOLLAR() { return getToken(SimpleQueryOptionsParser.DOLLAR, 0); }
 		public TerminalNode SCHEMAVERSION() { return getToken(SimpleQueryOptionsParser.SCHEMAVERSION, 0); }
 		public TerminalNode EQ() { return getToken(SimpleQueryOptionsParser.EQ, 0); }
 		public TerminalNode STAR() { return getToken(SimpleQueryOptionsParser.STAR, 0); }
+		public TerminalNode DOLLAR() { return getToken(SimpleQueryOptionsParser.DOLLAR, 0); }
 		public List<TerminalNode> UNRESERVED() { return getTokens(SimpleQueryOptionsParser.UNRESERVED); }
 		public TerminalNode UNRESERVED(int i) {
 			return getToken(SimpleQueryOptionsParser.UNRESERVED, i);
@@ -392,34 +409,42 @@ public class SimpleQueryOptionsParser extends Parser {
 		try {
 			enterOuterAlt(_localctx, 1);
 			{
-			setState(45);
-			match(DOLLAR);
-			setState(46);
+			setState(50);
+			_errHandler.sync(this);
+			_la = _input.LA(1);
+			if (_la==DOLLAR) {
+				{
+				setState(49);
+				match(DOLLAR);
+				}
+			}
+
+			setState(52);
 			match(SCHEMAVERSION);
-			setState(47);
+			setState(53);
 			match(EQ);
-			setState(54);
+			setState(60);
 			_errHandler.sync(this);
 			switch (_input.LA(1)) {
 			case STAR:
 				{
-				setState(48);
+				setState(54);
 				match(STAR);
 				}
 				break;
 			case UNRESERVED:
 				{
-				setState(50); 
+				setState(56); 
 				_errHandler.sync(this);
 				_la = _input.LA(1);
 				do {
 					{
 					{
-					setState(49);
+					setState(55);
 					match(UNRESERVED);
 					}
 					}
-					setState(52); 
+					setState(58); 
 					_errHandler.sync(this);
 					_la = _input.LA(1);
 				} while ( _la==UNRESERVED );
@@ -442,9 +467,9 @@ public class SimpleQueryOptionsParser extends Parser {
 	}
 
 	public static class SkipContext extends ParserRuleContext {
-		public TerminalNode DOLLAR() { return getToken(SimpleQueryOptionsParser.DOLLAR, 0); }
 		public TerminalNode SKIP_() { return getToken(SimpleQueryOptionsParser.SKIP_, 0); }
 		public TerminalNode EQ() { return getToken(SimpleQueryOptionsParser.EQ, 0); }
+		public TerminalNode DOLLAR() { return getToken(SimpleQueryOptionsParser.DOLLAR, 0); }
 		public List<TerminalNode> DIGIT() { return getTokens(SimpleQueryOptionsParser.DIGIT); }
 		public TerminalNode DIGIT(int i) {
 			return getToken(SimpleQueryOptionsParser.DIGIT, i);
@@ -470,72 +495,18 @@ public class SimpleQueryOptionsParser extends Parser {
 		try {
 			enterOuterAlt(_localctx, 1);
 			{
-			setState(56);
-			match(DOLLAR);
-			setState(57);
-			match(SKIP_);
-			setState(58);
-			match(EQ);
-			setState(60); 
+			setState(63);
 			_errHandler.sync(this);
 			_la = _input.LA(1);
-			do {
+			if (_la==DOLLAR) {
 				{
-				{
-				setState(59);
-				match(DIGIT);
+				setState(62);
+				match(DOLLAR);
 				}
-				}
-				setState(62); 
-				_errHandler.sync(this);
-				_la = _input.LA(1);
-			} while ( _la==DIGIT );
 			}
-		}
-		catch (RecognitionException re) {
-			_localctx.exception = re;
-			_errHandler.reportError(this, re);
-			_errHandler.recover(this, re);
-		}
-		finally {
-			exitRule();
-		}
-		return _localctx;
-	}
 
-	public static class TopContext extends ParserRuleContext {
-		public TerminalNode DOLLAR() { return getToken(SimpleQueryOptionsParser.DOLLAR, 0); }
-		public TerminalNode TOP() { return getToken(SimpleQueryOptionsParser.TOP, 0); }
-		public TerminalNode EQ() { return getToken(SimpleQueryOptionsParser.EQ, 0); }
-		public List<TerminalNode> DIGIT() { return getTokens(SimpleQueryOptionsParser.DIGIT); }
-		public TerminalNode DIGIT(int i) {
-			return getToken(SimpleQueryOptionsParser.DIGIT, i);
-		}
-		public TopContext(ParserRuleContext parent, int invokingState) {
-			super(parent, invokingState);
-		}
-		@Override public int getRuleIndex() { return RULE_top; }
-		@Override
-		public void enterRule(ParseTreeListener listener) {
-			if ( listener instanceof SimpleQueryOptionsListener ) ((SimpleQueryOptionsListener)listener).enterTop(this);
-		}
-		@Override
-		public void exitRule(ParseTreeListener listener) {
-			if ( listener instanceof SimpleQueryOptionsListener ) ((SimpleQueryOptionsListener)listener).exitTop(this);
-		}
-	}
-
-	public final TopContext top() throws RecognitionException {
-		TopContext _localctx = new TopContext(_ctx, getState());
-		enterRule(_localctx, 12, RULE_top);
-		int _la;
-		try {
-			enterOuterAlt(_localctx, 1);
-			{
-			setState(64);
-			match(DOLLAR);
 			setState(65);
-			match(TOP);
+			match(SKIP_);
 			setState(66);
 			match(EQ);
 			setState(68); 
@@ -565,27 +536,100 @@ public class SimpleQueryOptionsParser extends Parser {
 		return _localctx;
 	}
 
+	public static class TopContext extends ParserRuleContext {
+		public TerminalNode TOP() { return getToken(SimpleQueryOptionsParser.TOP, 0); }
+		public TerminalNode EQ() { return getToken(SimpleQueryOptionsParser.EQ, 0); }
+		public TerminalNode DOLLAR() { return getToken(SimpleQueryOptionsParser.DOLLAR, 0); }
+		public List<TerminalNode> DIGIT() { return getTokens(SimpleQueryOptionsParser.DIGIT); }
+		public TerminalNode DIGIT(int i) {
+			return getToken(SimpleQueryOptionsParser.DIGIT, i);
+		}
+		public TopContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_top; }
+		@Override
+		public void enterRule(ParseTreeListener listener) {
+			if ( listener instanceof SimpleQueryOptionsListener ) ((SimpleQueryOptionsListener)listener).enterTop(this);
+		}
+		@Override
+		public void exitRule(ParseTreeListener listener) {
+			if ( listener instanceof SimpleQueryOptionsListener ) ((SimpleQueryOptionsListener)listener).exitTop(this);
+		}
+	}
+
+	public final TopContext top() throws RecognitionException {
+		TopContext _localctx = new TopContext(_ctx, getState());
+		enterRule(_localctx, 12, RULE_top);
+		int _la;
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(73);
+			_errHandler.sync(this);
+			_la = _input.LA(1);
+			if (_la==DOLLAR) {
+				{
+				setState(72);
+				match(DOLLAR);
+				}
+			}
+
+			setState(75);
+			match(TOP);
+			setState(76);
+			match(EQ);
+			setState(78); 
+			_errHandler.sync(this);
+			_la = _input.LA(1);
+			do {
+				{
+				{
+				setState(77);
+				match(DIGIT);
+				}
+				}
+				setState(80); 
+				_errHandler.sync(this);
+				_la = _input.LA(1);
+			} while ( _la==DIGIT );
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
 	public static final String _serializedATN =
-		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\23K\4\2\t\2\4\3\t"+
+		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\23U\4\2\t\2\4\3\t"+
 		"\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7\4\b\t\b\3\2\3\2\3\2\7\2\24\n\2\f\2"+
-		"\16\2\27\13\2\3\3\3\3\3\3\3\3\3\3\5\3\36\n\3\3\4\3\4\3\4\3\4\3\4\3\5\3"+
-		"\5\3\5\3\5\5\5)\n\5\3\5\6\5,\n\5\r\5\16\5-\3\6\3\6\3\6\3\6\3\6\6\6\65"+
-		"\n\6\r\6\16\6\66\5\69\n\6\3\7\3\7\3\7\3\7\6\7?\n\7\r\7\16\7@\3\b\3\b\3"+
-		"\b\3\b\6\bG\n\b\r\b\16\bH\3\b\2\2\t\2\4\6\b\n\f\16\2\2\2N\2\20\3\2\2\2"+
-		"\4\35\3\2\2\2\6\37\3\2\2\2\b$\3\2\2\2\n/\3\2\2\2\f:\3\2\2\2\16B\3\2\2"+
-		"\2\20\25\5\4\3\2\21\22\7\21\2\2\22\24\5\4\3\2\23\21\3\2\2\2\24\27\3\2"+
-		"\2\2\25\23\3\2\2\2\25\26\3\2\2\2\26\3\3\2\2\2\27\25\3\2\2\2\30\36\5\6"+
-		"\4\2\31\36\5\b\5\2\32\36\5\n\6\2\33\36\5\f\7\2\34\36\5\16\b\2\35\30\3"+
-		"\2\2\2\35\31\3\2\2\2\35\32\3\2\2\2\35\33\3\2\2\2\35\34\3\2\2\2\36\5\3"+
-		"\2\2\2\37 \7\16\2\2 !\7\5\2\2!\"\7\17\2\2\"#\7\4\2\2#\7\3\2\2\2$%\7\16"+
-		"\2\2%&\7\6\2\2&(\7\17\2\2\')\7\r\2\2(\'\3\2\2\2()\3\2\2\2)+\3\2\2\2*,"+
-		"\7\23\2\2+*\3\2\2\2,-\3\2\2\2-+\3\2\2\2-.\3\2\2\2.\t\3\2\2\2/\60\7\16"+
-		"\2\2\60\61\7\7\2\2\618\7\17\2\2\629\7\20\2\2\63\65\7\3\2\2\64\63\3\2\2"+
-		"\2\65\66\3\2\2\2\66\64\3\2\2\2\66\67\3\2\2\2\679\3\2\2\28\62\3\2\2\28"+
-		"\64\3\2\2\29\13\3\2\2\2:;\7\16\2\2;<\7\b\2\2<>\7\17\2\2=?\7\23\2\2>=\3"+
-		"\2\2\2?@\3\2\2\2@>\3\2\2\2@A\3\2\2\2A\r\3\2\2\2BC\7\16\2\2CD\7\t\2\2D"+
-		"F\7\17\2\2EG\7\23\2\2FE\3\2\2\2GH\3\2\2\2HF\3\2\2\2HI\3\2\2\2I\17\3\2"+
-		"\2\2\n\25\35(-\668@H";
+		"\16\2\27\13\2\3\3\3\3\3\3\3\3\3\3\5\3\36\n\3\3\4\5\4!\n\4\3\4\3\4\3\4"+
+		"\3\4\3\5\5\5(\n\5\3\5\3\5\3\5\5\5-\n\5\3\5\6\5\60\n\5\r\5\16\5\61\3\6"+
+		"\5\6\65\n\6\3\6\3\6\3\6\3\6\6\6;\n\6\r\6\16\6<\5\6?\n\6\3\7\5\7B\n\7\3"+
+		"\7\3\7\3\7\6\7G\n\7\r\7\16\7H\3\b\5\bL\n\b\3\b\3\b\3\b\6\bQ\n\b\r\b\16"+
+		"\bR\3\b\2\2\t\2\4\6\b\n\f\16\2\2\2]\2\20\3\2\2\2\4\35\3\2\2\2\6 \3\2\2"+
+		"\2\b\'\3\2\2\2\n\64\3\2\2\2\fA\3\2\2\2\16K\3\2\2\2\20\25\5\4\3\2\21\22"+
+		"\7\21\2\2\22\24\5\4\3\2\23\21\3\2\2\2\24\27\3\2\2\2\25\23\3\2\2\2\25\26"+
+		"\3\2\2\2\26\3\3\2\2\2\27\25\3\2\2\2\30\36\5\6\4\2\31\36\5\b\5\2\32\36"+
+		"\5\n\6\2\33\36\5\f\7\2\34\36\5\16\b\2\35\30\3\2\2\2\35\31\3\2\2\2\35\32"+
+		"\3\2\2\2\35\33\3\2\2\2\35\34\3\2\2\2\36\5\3\2\2\2\37!\7\16\2\2 \37\3\2"+
+		"\2\2 !\3\2\2\2!\"\3\2\2\2\"#\7\5\2\2#$\7\17\2\2$%\7\4\2\2%\7\3\2\2\2&"+
+		"(\7\16\2\2\'&\3\2\2\2\'(\3\2\2\2()\3\2\2\2)*\7\6\2\2*,\7\17\2\2+-\7\r"+
+		"\2\2,+\3\2\2\2,-\3\2\2\2-/\3\2\2\2.\60\7\23\2\2/.\3\2\2\2\60\61\3\2\2"+
+		"\2\61/\3\2\2\2\61\62\3\2\2\2\62\t\3\2\2\2\63\65\7\16\2\2\64\63\3\2\2\2"+
+		"\64\65\3\2\2\2\65\66\3\2\2\2\66\67\7\7\2\2\67>\7\17\2\28?\7\20\2\29;\7"+
+		"\3\2\2:9\3\2\2\2;<\3\2\2\2<:\3\2\2\2<=\3\2\2\2=?\3\2\2\2>8\3\2\2\2>:\3"+
+		"\2\2\2?\13\3\2\2\2@B\7\16\2\2A@\3\2\2\2AB\3\2\2\2BC\3\2\2\2CD\7\b\2\2"+
+		"DF\7\17\2\2EG\7\23\2\2FE\3\2\2\2GH\3\2\2\2HF\3\2\2\2HI\3\2\2\2I\r\3\2"+
+		"\2\2JL\7\16\2\2KJ\3\2\2\2KL\3\2\2\2LM\3\2\2\2MN\7\t\2\2NP\7\17\2\2OQ\7"+
+		"\23\2\2PO\3\2\2\2QR\3\2\2\2RP\3\2\2\2RS\3\2\2\2S\17\3\2\2\2\17\25\35 "+
+		"\',\61\64<>AHKR";
 	public static final ATN _ATN =
 		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
 	static {
